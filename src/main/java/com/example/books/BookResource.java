2e67f0db11511f0385de42c239442ebe49e25ca9
SaKu <sakuofficial@gmail.com>
2020-11-10 05:28:51 +0530

    Create BookResouce Class

---
+++ src/main/java/com/example/books/BookResource.java
@@ -0,0 +1,22 @@
+package com.example.books;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookResource {
+	@Autowired
+	private BookService  bookservice;
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks() {
+		
+		return bookservice.findAllBooks();
+		
+	}
+	
+	
+}

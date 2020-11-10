9cc44323e1cb055cf23952912345735addbe1b42
SaKu <sakuofficial@gmail.com>
2020-11-10 05:49:37 +0530

    Add Book Rest services

--- src/main/java/com/example/books/BookResource.java
+++ src/main/java/com/example/books/BookResource.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,17 @@ public class BookResource {
 		
 	}
 	
+	@GetMapping("/books/{bookId}")
+	public Book retrieveBook(@PathVariable int bookId) {
+		
+		return bookservice.findBook(bookId);
+		
+	}
 	
+	@PostMapping("/books")
+	public void createBook(@RequestBody Book book) {
+		
+		Book saveBook = bookservice.addBook(book);
+		
+	}
 }
